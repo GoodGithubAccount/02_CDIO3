92930b37de6f127acc0ae50078fcdbe1f604b8f0
Sebastian Goldbach <Sig.goldbach@gmail.com>
2021-11-08 15:12:29 +0100

    Account class added

---
+++ src/Game/Account.java
@@ -0,0 +1,29 @@
+package Game;
+
+public class Account {
+    // Our start balance and balance.
+    private final int startbal = 1000;
+    private int balance;
+
+    // Sets balance to our startbalance
+    public Account() {
+        this.balance = startbal;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    // Updates the balance and also makes sure it doesn't go below 0
+    public void newBalance (int ubalance){
+        this.balance += ubalance;
+
+        if(this.balance < 0){
+            this.balance = 0;
+        }
+    }
+}
