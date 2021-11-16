d895c85747bb9ee75192a05fd9817d71a5d8265a
Sebastian Ellegaard <sebynoah@gmail.com>
2021-11-16 16:38:32 +0100

    Skrevet alle kort ind i en txt fil, med forskellige typer af kort, værdien samt hvor meget man skal bevæge sig

--- src/Game/ChanceCardGenerate.java
+++ src/Game/ChanceCardGenerate.java
@@ -7,8 +7,9 @@ public class ChanceCardGenerate {
 
     public static void main(String[] args) throws IOException {
         String[] text = ChancekortReader.text();
-        ChanceMoneyOrMove c16 = new ChanceMoneyOrMove("Modtag 2", text[0], 16, 2, 0, 0);
+        ChanceMoneyOrMove c16 = new ChanceMoneyOrMove("Modtag 2", text[16], 16, 2, 0, 0);
         ChanceMoneyOrMove c2 = new ChanceMoneyOrMove("Modtag 2", text[1], 2, 2, 0, 0);
+        ChanceMoneyOrMove c1 = new ChanceMoneyOrMove("Giv Bil kort",text[0],1,0,0,0);
         System.out.println(c2.toString());
         System.out.println(c16.getText());
         System.out.println(c2.getText());
