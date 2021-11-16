b34f1f7587de4353254800e9b1d27d268f4e4894
Sebastian Goldbach <Sig.goldbach@gmail.com>
2021-11-16 15:57:28 +0100

    PLayerklasse

--- src/Game/Player.java
+++ src/Game/Player.java
@@ -1,5 +1,40 @@
 package Game;
 
 public class Player {
+    String name;
+    int playernumber;
+    Account ac;
+    private static int counter=1;
 
+    public Player(String name) {
+        this.name = name;
+        this.playernumber = counter;
+        counter++;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPlayernumber() {
+        return playernumber;
+    }
+
+    public void setPlayernumber(int playernumber) {
+        this.playernumber = playernumber;
+    }
+
+    public Account getAc() {
+        return ac;
+    }
+
+    public void setAc(Account ac) {
+        this.ac = ac;
+    }
 }
+
+
