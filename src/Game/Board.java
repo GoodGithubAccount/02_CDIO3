ad366b7505681f066e5b5cd0bba965acb558fd29
Valdemar Nielsen <v.nielsen@live.dk>
2021-11-23 18:33:23 +0100

    Chancedatabase

--- src/Game/Board.java
+++ src/Game/Board.java
@@ -7,30 +7,40 @@ public class Board {
     String fieldDatabase;
     String chanceDatabase;
     Field[] myFields;
+    Chance[] myChance;
 
-    public Board(String fieldDatabase, String chanceDatabase, int boardSize){
+    public Board(String fieldDatabase, String chanceDatabase, int boardSize) {
         this.fieldDatabase = fieldDatabase;
         this.chanceDatabase = chanceDatabase;
         this.boardSize = boardSize;
         this.myFields = new Field[boardSize];
     }
 
-    private String loadFromTXT(String txtDatabase){
+    private String loadFromTXT(String txtDatabase) {
         TXTReader myReader = new TXTReader(txtDatabase);
         return myReader.readTXTFile();
     }
 
-    public void generateBoard(){
+    public void generateBoard() {
         String Temp = loadFromTXT(fieldDatabase);
 
         String[] fields = Temp.split("\n");
 
-        for(int i = 0; i < boardSize; i++){
+        for (int i = 0; i < boardSize; i++) {
             String[] fieldProperties = fields[i].split("-");
 
             myFields[i] = new Field(Field.fieldType.valueOf(fieldProperties[0]), fieldProperties[1], Integer.parseInt(fieldProperties[2]), fieldProperties[3]);
         }
     }
 
-    public Field[] getMyFields() { return myFields; }
+    public void generateChance() {
+        String Temp = loadFromTXT(chanceDatabase);
+
+        String[] chance = Temp.split("\n");
+myChance = new Chance[chance.length];
+        for (int i = 0; i <chance.length; i++) {
+            String[] chanceProperties = chance[i].split("-");
+
+        }
+    }
 }
